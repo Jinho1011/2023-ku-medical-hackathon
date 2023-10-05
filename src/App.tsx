839e05928005ecb07b1537acb94b3bf915a8ca12
Jeon <gino9940@gmail.com>
2023-10-05 15:01:26 +0900

    feat: style 4 split view

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,23 @@
 import React from "react";
 import RTSPPlayer from "./components/RTSPPlayer";
 
+import "./App.css";
+
 function App() {
   return (
     <div className="App">
-      <RTSPPlayer />
+      <header>
+        <h1>Daila</h1>
+        <span>킹갓제너럴어쩌구저쩌구메디컬해커톤아자아자화이팅</span>
+      </header>
+      <main>
+        <div className="container">
+          <RTSPPlayer />
+          <RTSPPlayer />
+          <RTSPPlayer />
+          <RTSPPlayer />
+        </div>
+      </main>
     </div>
   );
 }
